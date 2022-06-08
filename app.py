5260f6063154081786b894d8f5f887c092742848
Dave Barragan <itsdaveba@gmail.com>
2022-06-08 17:51:29 -0500

    docker compose

---
+++ app.py
@@ -0,0 +1,64 @@
+import os
+import mysql.connector
+import json
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/widgets')
+def get_widgets():
+    mydb = mysql.connector.connect(
+        host=os.environ['MYSQL_HOST'],
+        user=os.environ['MYSQL_USER'],
+        password=os.environ['MYSQL_PASSWORD'],
+        database=os.environ['MYSQL_DB']
+    )
+    cursor = mydb.cursor()
+
+
+    cursor.execute("SELECT * FROM widgets")
+
+    row_headers=[x[0] for x in cursor.description] #this will extract row headers
+
+    results = cursor.fetchall()
+    json_data=[]
+    for result in results:
+        json_data.append(dict(zip(row_headers,result)))
+
+    cursor.close()
+
+    return json.dumps(json_data)
+
+@app.route('/initdb')
+def db_init():
+    mydb = mysql.connector.connect(
+        host=os.environ['MYSQL_HOST'],
+        user=os.environ['MYSQL_USER'],
+        password=os.environ['MYSQL_PASSWORD']
+    )
+    cursor = mydb.cursor()
+
+    cursor.execute("DROP DATABASE IF EXISTS inventory")
+    cursor.execute("CREATE DATABASE inventory")
+    cursor.close()
+
+    mydb = mysql.connector.connect(
+        host=os.environ['MYSQL_HOST'],
+        user=os.environ['MYSQL_USER'],
+        password=os.environ['MYSQL_PASSWORD'],
+        database=os.environ['MYSQL_DB']
+    )
+    cursor = mydb.cursor()
+
+    cursor.execute("DROP TABLE IF EXISTS widgets")
+    cursor.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
+    cursor.close()
+
+    return 'init database'
+
+if __name__ == "__main__":
+    app.run(host ='0.0.0.0', debug=True)
